808c50cc504d2f038b7d719b3d4854852e104d70
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2023-02-14 06:43:23 +0700

    success handle expired token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,7 +4,14 @@ import jwt from "jsonwebtoken";
 export const auth = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.sendStatus(401);
-  const decoded = jwt.verify(token, process.env.ACCESS_JWT_KEY!);
+
+  // if token expired
+  const time: any = jwt.decode(token);
+  if (Date.now() >= time.exp * 1000) {
+    return res.status(403).json({ msg: "token expired" });
+  }
+
+  const decoded: any = jwt.verify(token, process.env.ACCESS_JWT_KEY!);
   if (!decoded) return res.sendStatus(403);
   req.app.locals.user = decoded;
   next();
