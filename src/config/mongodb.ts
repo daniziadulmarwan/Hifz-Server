e9fb9e8226b5e634e2f2f8d800e923019f2ccb96
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2023-02-15 12:25:32 +0700

    add mongo online

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -3,8 +3,13 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
 mongoose.set("strictPopulate", false);
 
+const mongoOnline =
+  "mongodb+srv://dani913:JtJWHpM0298TLWhz@cluster0.ubiwmc7.mongodb.net/hfiz_book?retryWrites=true&w=majority";
+
+const mongoOffline = "mongodb://127.0.0.1:27017/hfiz_book";
+
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/hfiz_book");
+  await mongoose.connect(mongoOnline);
 }
 
 export default main;
