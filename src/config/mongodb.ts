486b805bed9839254181b47e60acfa41984bcb28
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2023-02-16 10:38:41 +0700

    add

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -9,7 +9,7 @@ const mongoOnline =
 const mongoOffline = "mongodb://127.0.0.1:27017/hfiz_book";
 
 async function main() {
-  await mongoose.connect(mongoOffline);
+  await mongoose.connect(mongoOnline);
 }
 
 export default main;
